d0ee6135eff15fe0d84d9132b2df356a0caee9a6
Gabriel Lemus <glemus.stuart@gmail.com>
2021-12-30 12:33:23 -0600

    Refactor app

--- src/App.js
+++ src/App.js
@@ -1,15 +1,9 @@
-import Canvas from './components/atoms/Canvas';
-import ButtonsHeader from './components/molecules/ButtonsHeader';
-import Controls from './components/molecules/Controls';
-import Footer from './components/molecules/Footer';
+import FractalDisplay from './components/organisms/FractalDisplay';
 
 function App() {
   return (
     <div className="App">
-      <ButtonsHeader />
-      <Canvas />
-      <Controls iteration={0} />
-      <Footer />
+      <FractalDisplay />
     </div>
   );
 }
