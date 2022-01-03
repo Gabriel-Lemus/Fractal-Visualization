1797068ec9846c9d5a1e6db260f4c533dbe0653d
Gabriel Lemus <glemus.stuart@gmail.com>
2022-01-02 20:51:26 -0600

    Update implementation of fractal button component

--- src/components/atoms/FractalButton.jsx
+++ src/components/atoms/FractalButton.jsx
@@ -1,7 +1,13 @@
 import React from 'react';
 
 function FractalButton(props) {
-  return <button className="fractal-button">{props.children}</button>;
+  return (
+    <button
+      className={`fractal-button ${props.active ? 'active' : 'inactive'}`}
+    >
+      {props.children}
+    </button>
+  );
 }
 
 export default FractalButton;
