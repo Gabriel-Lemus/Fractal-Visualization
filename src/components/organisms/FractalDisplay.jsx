7c1bd71ecb1397bedb2062bcc45b195b67c1c4b2
Gabriel Lemus <glemus.stuart@gmail.com>
2022-01-02 21:08:58 -0600

    Update display implementation

--- src/components/organisms/FractalDisplay.jsx
+++ src/components/organisms/FractalDisplay.jsx
@@ -1,17 +1,32 @@
-import React from 'react';
+import React, { createContext, useEffect, useState } from 'react';
 import Canvas from '../molecules/Canvas';
 import ButtonsHeader from '../molecules/ButtonsHeader';
 import Controls from '../molecules/Controls';
 import Footer from '../molecules/Footer';
+import helpers from '../../helpers/helpers';
+
+export const FractalsContext = createContext();
 
 function FractalDisplay() {
+  const [fractals, setFractals] = useState(helpers.getFractals());
+  const [activeFractal, setActiveFractal] = useState({});
+
+  useEffect(() => {
+    setActiveFractal(fractals[0]);
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
   return (
-    <>
-      <ButtonsHeader />
-      <Canvas />
+    <FractalsContext.Provider
+      value={{ fractals, setFractals, activeFractal, setActiveFractal }}
+    >
+      <ButtonsHeader>
+        {fractals.map((fractal) => [fractal.name, fractal.active])}
+      </ButtonsHeader>
+      <Canvas fractal={activeFractal} />
       <Controls iteration={0} />
       <Footer />
-    </>
+    </FractalsContext.Provider>
   );
 }
 
