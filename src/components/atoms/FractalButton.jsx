3a8fe3b5366955fba3810b9320be0aad833bf4c3
Gabriel Lemus <glemus.stuart@gmail.com>
2022-01-02 21:09:40 -0600

    Implement initial fractal button logic

--- src/components/atoms/FractalButton.jsx
+++ src/components/atoms/FractalButton.jsx
@@ -1,9 +1,30 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import { FractalsContext } from '../organisms/FractalDisplay';
 
 function FractalButton(props) {
+  const context = useContext(FractalsContext);
+  const fractals = context.fractals;
+  const setFractals = context.setFractals;
+  const setActiveFractal = context.setActiveFractal;
+
+  // Change the active fractal
+  const handleToggleFractalDisplay = () => {
+    if (!props.active) {
+      let newFractals = [];
+
+      for (let i = 0; i < fractals.length; i++) {
+        newFractals.push({ ...fractals[i], active: i === props.index });
+      }
+
+      setFractals(newFractals);
+      setActiveFractal(newFractals[props.index]);
+    }
+  };
+
   return (
     <button
       className={`fractal-button ${props.active ? 'active' : 'inactive'}`}
+      onClick={handleToggleFractalDisplay}
     >
       {props.children}
     </button>
