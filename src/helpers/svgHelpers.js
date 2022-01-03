3fdbb120b48355cc9859efae43a8020648d7d74d
Gabriel Lemus <glemus.stuart@gmail.com>
2022-01-03 10:45:56 -0600

    Add helper to draw the outline of the SVG element

--- src/helpers/svgHelpers.js
+++ src/helpers/svgHelpers.js
@@ -26,9 +26,31 @@ const getSvgDimensions = () => {
   return { width, height };
 };
 
+/*
+ * Function to draw an outline to the SVG element
+ */
+const drawOutline = () => {
+  const { width, height } = getSvgDimensions();
+
+  const svg = document.getElementById('fractal-canvas');
+  const outline = document.createElementNS(
+    'http://www.w3.org/2000/svg',
+    'path'
+  );
+  outline.setAttribute(
+    'd',
+    `M 0 0 L 0 ${height} L ${width} ${height} L ${width} 0 L 0 0`
+  );
+  outline.setAttribute('stroke', 'black');
+  outline.setAttribute('stroke-width', '1');
+  outline.setAttribute('fill', 'none');
+  svg.appendChild(outline);
+};
+
 const svgHelpers = {
   getSvgPolyline,
   getSvgDimensions,
+  drawOutline,
 };
 
 export default svgHelpers;
