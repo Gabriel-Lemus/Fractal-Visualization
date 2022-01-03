408476c5a020e6bae62612afe79333694c72985c
Gabriel Lemus <glemus.stuart@gmail.com>
2022-01-02 18:20:47 -0600

    Add color palette

---
+++ src/helpers/helpers.js
@@ -0,0 +1,14 @@
+// Color palette
+const PALETTE = {
+  darkBlue: '#22577e',
+  blue: '#5584ac',
+  lightCyan: '#95d1cc',
+  beige: '#f6f2d4',
+  white: '#ffffff',
+};
+
+const helpers = {
+  PALETTE,
+};
+
+export default helpers;
