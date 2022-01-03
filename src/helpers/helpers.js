a502f785fb290ef0f3d9a1305d2bc8bb8fa00a87
Gabriel Lemus <glemus.stuart@gmail.com>
2022-01-02 18:21:52 -0600

    Add initial implementation of function to get the fractals

--- src/helpers/helpers.js
+++ src/helpers/helpers.js
@@ -7,8 +7,32 @@ const PALETTE = {
   white: '#ffffff',
 };
 
+/*
+ * Function to return an array of the fractals to be displayed
+ */
+const getFractals = () => {
+  return [
+    {
+      name: 'Copo de Nieve de Koch',
+      svg: 0,
+      active: true,
+    },
+    {
+      name: 'Triángulo de Sierpinski',
+      svg: 0,
+      active: false,
+    },
+    {
+      name: 'Esponja de Menger',
+      svg: 0,
+      active: false,
+    },
+  ];
+};
+
 const helpers = {
   PALETTE,
+  getFractals,
 };
 
 export default helpers;
