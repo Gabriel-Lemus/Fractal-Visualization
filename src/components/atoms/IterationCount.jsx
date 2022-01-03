557378465975628927e1b16afe42d408fd522e06
Gabriel Lemus <glemus.stuart@gmail.com>
2022-01-02 22:10:22 -0600

    Add current fractal's iteration

--- src/components/atoms/IterationCount.jsx
+++ src/components/atoms/IterationCount.jsx
@@ -1,9 +1,12 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import { FractalsContext } from '../organisms/FractalDisplay';
 
 function IterationCount() {
+  const { activeFractal } = useContext(FractalsContext);
+  
   return (
     <section className="iteration-count">
-      <p className="iteration">Iteración: </p>
+      <p className="iteration">Iteración: {activeFractal.iterations}</p>
     </section>
   );
 }
