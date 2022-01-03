eb3b4b74b36f13e6fc97983bb8c9676d0ac0cb5c
Gabriel Lemus <glemus.stuart@gmail.com>
2022-01-02 19:01:59 -0600

    Add Koch Snowflake logic file

---
+++ src/helpers/fractals/kochSnowflake.js
@@ -0,0 +1,19 @@
+import helpers from '../helpers';
+import svgHelpers from '../svgHelpers';
+
+const getKochSnowflake = (iterations) => {
+  return svgHelpers.getSvgPolyline(
+    [
+      { x: 0, y: 0 },
+      { x: 10, y: 1 },
+    ],
+    helpers.PALETTE.darkBlue,
+    1
+  );
+};
+
+const kochSnowflake = {
+  getKochSnowflake,
+};
+
+export default kochSnowflake;
