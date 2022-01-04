c34e13889cd8aa3c5e554750ac1decce151c9042
Gabriel Lemus <glemus.stuart@gmail.com>
2022-01-04 11:55:45 -0600

    Refactor code with JSDocs

--- src/helpers/fractals/sierpinskiTriangle.js
+++ src/helpers/fractals/sierpinskiTriangle.js
@@ -1,19 +1,32 @@
 import helpers from '../helpers';
 import svgHelpers from '../svgHelpers';
 
+/**
+ * Function that returns an array of SVG paths that represent the Sierpinski Triangle fractal
+ * @param {Number} iteration - The iteration of the fractal
+ * @returns {Array.<SVGPathElement>}
+ */
 const getSierpinskiTriangle = (iteration) => {
-  return svgHelpers.getSvgPolyline(
-    [
-      { x: 0, y: 0 },
-      { x: 10, y: 1 },
-    ],
-    helpers.PALETTE.darkBlue,
-    1
+  const { width, height } = svgHelpers.getSvgDimensions();
+  const circle = document.createElementNS(
+    'http://www.w3.org/2000/svg',
+    'circle'
   );
+  circle.setAttribute('cx', width / 2);
+  circle.setAttribute('cy', height / 2);
+  circle.setAttribute('r', height / 4);
+  circle.setAttribute('fill', 'none');
+  circle.setAttribute('stroke', 'red');
+  circle.setAttribute('stroke-width', '2');
+
+  return [circle];
 };
 
-const kochSnowflake = {
+/**
+ * Object that represents the Sierpinski Triangle fractal
+ */
+const sierpinskiTriangle = {
   getSierpinskiTriangle,
 };
 
-export default kochSnowflake;
+export default sierpinskiTriangle;
