0351a8a6ae8d24efce349efb2a5271cd5810838e
Gabriel Lemus <glemus.stuart@gmail.com>
2022-01-04 12:47:58 -0600

    Add function to convert from radians to degrees

--- src/helpers/mathHelpers.js
+++ src/helpers/mathHelpers.js
@@ -8,10 +8,20 @@ const degreesToRadians = (degrees) => {
 };
 
 /**
+ * Function that converts from radians to degrees
+ * @param {Number} radians - Radians to convert
+ * @returns {Number} Degrees
+ */
+const radiansToDegrees = (radians) => {
+  return radians * (180 / Math.PI);
+};
+
+/**
  * Object that contains the math functions
  */
 const mathHelpers = {
   degreesToRadians,
+  radiansToDegrees,
 };
 
 export default mathHelpers;
