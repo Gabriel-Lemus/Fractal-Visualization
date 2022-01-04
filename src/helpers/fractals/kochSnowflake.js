6d81f0d0779200113436596b24c2109a4cf41411
Gabriel Lemus <glemus.stuart@gmail.com>
2022-01-04 11:55:37 -0600

    Refactor code with JSDocs

--- src/helpers/fractals/kochSnowflake.js
+++ src/helpers/fractals/kochSnowflake.js
@@ -1,17 +1,30 @@
 import helpers from '../helpers';
 import svgHelpers from '../svgHelpers';
 
+/**
+ * Function that returns an array of SVG paths that represent the Koch Snowflake fractal
+ * @param {Number} iteration - The iteration of the fractal
+ * @returns {Array.<SVGPathElement>}
+ */
 const getKochSnowflake = (iteration) => {
-  return svgHelpers.getSvgPolyline(
-    [
-      { x: 0, y: 0 },
-      { x: 10, y: 1 },
-    ],
-    helpers.PALETTE.darkBlue,
-    1
+  const { width, height } = svgHelpers.getSvgDimensions();
+  const circle = document.createElementNS(
+    'http://www.w3.org/2000/svg',
+    'circle'
   );
+  circle.setAttribute('cx', width / 2);
+  circle.setAttribute('cy', height / 2);
+  circle.setAttribute('r', height / 3);
+  circle.setAttribute('fill', 'none');
+  circle.setAttribute('stroke', 'black');
+  circle.setAttribute('stroke-width', '2');
+
+  return [circle];
 };
 
+/**
+ * Object that represents the Koch Snowflake fractal
+ */
 const kochSnowflake = {
   getKochSnowflake,
 };
