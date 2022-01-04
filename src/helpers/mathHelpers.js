e66e57c718c6da5c0e2e6a2c6b977ba891ca6eb4
Gabriel Lemus <glemus.stuart@gmail.com>
2022-01-04 14:31:22 -0600

    Add function to get the height of an equilateral triangle

--- src/helpers/mathHelpers.js
+++ src/helpers/mathHelpers.js
@@ -17,11 +17,23 @@ const radiansToDegrees = (radians) => {
 };
 
 /**
+ * Function to get the height of an equilateral triangle based on the given sidelength
+ * @param {Number} sidelength - Sidelength of the equilateral triangle
+ * @returns {Number} Height of the equilateral triangle
+ */
+const getEquilateralTriangleHeight = (sidelength) => {
+  const height = sidelength / 2 * Math.sqrt(3);
+
+  return height;
+};
+
+/**
  * Object that contains the math functions
  */
 const mathHelpers = {
   degreesToRadians,
   radiansToDegrees,
+  getEquilateralTriangleHeight,
 };
 
 export default mathHelpers;
